dc7a152016a036b2252b4fba2dcdc5d46d5d085e
Gabriel <gabylopez4470@gmail.com>
2022-11-27 22:01:03 -0300

    aca el nombre q quieras poner

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
-//alert()
-document.write("<p>----------------------</p>")
-document.write("<p>MONEDAS</p>")
-document.write("<p> USD (Dolar Estadounidense)</p>")
-document.write("<p> CLP (Peso Chileno)</p>")
-document.write("<p> EUR (Euro)</p>")
-document.write("<p> ARS (Peso Argentino)</p>")
 let valor1
 let valor2
 let cantidad
 let moneda1
 let moneda2
+let x = 0
 const monedas = [{ moneda: 'USD', valor: 1 },
-{ moneda: 'EUR', valor: 1 },
-{ moneda: 'ARS', valor: 159.72 },
-{ moneda: 'CLP', valor: 905 }]
+{ moneda: 'EUR', valor: 0.963695 },
+{ moneda: 'ARS', valor: 315 },
+{ moneda: 'CLP', valor: 912.25}, 
+{ moneda: 'MXN', valor: 19.3527},
+{ moneda: 'COP', valor: 4883.74},
+{ moneda: 'YEN', valor: 139.28}]
 function buscarmoneda() {
-    moneda1 = prompt("CONVERTIR (USD/EUR/ARS/CLP)")
-    moneda2 = prompt(" A (USD/EUR/ARS/CLP)")
-    cantidad = Number(prompt(" CANTIDAD"))
+    
     for (const moneda of monedas) {
         if (moneda.moneda == moneda1.toUpperCase()) {
             valor1 = moneda.valor
@@ -27,22 +22,47 @@ function buscarmoneda() {
         }
     }
     resultado = (valor2 / valor1) * cantidad
-    return resultado
+    resultadofinal = resultado.toFixed(2)
+    return resultadofinal
 }
-buscarmoneda()
-document.write("<p>" + resultado.toFixed(2) +" "+moneda2.toUpperCase() + "</p>")
-
-
-let otra = prompt("¿Desea otra operación? (SI/NO)")
-if (otra.toUpperCase() == "NO") {
-    document.write("<p>Gracias por utilizar el conversor</p>")
-    document.write("<p>=====================================</p>")
-} else {
-    while (otra.toUpperCase() == "SI") {
-        buscarmoneda()
-        document.write("<p>" + resultado.toFixed(2) +" "+moneda2.toUpperCase() + "</p>")
-        otra = prompt("¿Desea otra operación? (SI/NO)")
+function removeAllChilds(a)
+ {
+ var a=document.getElementById(a);
+ while(a.hasChildNodes())
+	a.removeChild(a.firstChild);	
+ }
+const contenedor = document.getElementById("res")
+const btn = document.getElementById("calcular")
+btn.addEventListener('click',(evt)=>{
+    evt.preventDefault()
+    removeAllChilds("res")
+    moneda1 = document.getElementById("monedaUno").value
+    moneda2 = document.getElementById("monedaDos").value
+    cantidad = document.getElementById("cantidad").value
+    buscarmoneda()
+    let items= cantidad + " " + moneda1 + " a " + moneda2 + " = " +resultadofinal+moneda2
+    localStorage.setItem("resultados"+x,items)
+    x+=1
+    if(localStorage.length != 0){
+        for(let i=0; i<localStorage.length; i+=1 ){
+            const elementop = document.createElement("p")
+            elementop.innerHTML = localStorage.getItem("resultados"+i)
+            contenedor.appendChild(elementop)
+        }
     }
-    document.write("<p>Gracias por utilizar el conversor</p>")
-    
-}   
+
+})
+if(localStorage.length != 0){
+    for(let i=0; i<localStorage.length; i+=1 ){
+        const elementop = document.createElement("p")
+        elementop.innerHTML = localStorage.getItem("resultados"+i)
+        contenedor.appendChild(elementop)
+    }
+}
+
+const limpiar = document.getElementById("botonLimpiar")
+limpiar.addEventListener('click', (evt)=>{
+    localStorage.clear()
+    removeAllChilds("res")
+    x = 0
+})
