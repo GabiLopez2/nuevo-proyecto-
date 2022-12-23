6c1f3ad0a29b593c34d25a53779cfcf2f4124e94
Gabriel <gabylopez4470@gmail.com>
2022-12-23 19:49:15 -0300

    correccion de herrores

--- js/main.js
+++ js/main.js
@@ -1,46 +1,67 @@
-let valor1
-let valor2
-let cantidad
-let moneda1
-let moneda2
+const moneda1 = document.querySelector("#monedaUno")
+const moneda2 = document.querySelector("#monedaDos")
+const cantidad = document.querySelector("#cantidad")
+const cantidad2= document.querySelector("#cantidad2")
 let x = 0
-const monedas = [{ moneda: 'USD', valor: 1 },
-{ moneda: 'EUR', valor: 0.963695 },
-{ moneda: 'ARS', valor: 315 },
-{ moneda: 'CLP', valor: 912.25}, 
-{ moneda: 'MXN', valor: 19.3527},
-{ moneda: 'COP', valor: 4883.74},
-{ moneda: 'YEN', valor: 139.28}]
-function buscarmoneda() {
-    
-    for (const moneda of monedas) {
-        if (moneda.moneda == moneda1.toUpperCase()) {
-            valor1 = moneda.valor
-        }
-        if (moneda.moneda == moneda2.toUpperCase()) {
-            valor2 = moneda.valor
-        }
-    }
-    resultado = (valor2 / valor1) * cantidad
-    resultadofinal = resultado.toFixed(2)
-    return resultadofinal
+
+
+async function valormoneda(lur){
+    const result = await fetch(lur)
+    const dato = await result.json()
+    return dato
 }
+
+function render(valori){
+    cantidad2.setAttribute("value",valori.result.toFixed(2))
+    document.getElementById("cantidad2").value = valori.result.toFixed(2)
+    
+}
+function render2(valori){
+    cantidad.setAttribute("value",valori.result.toFixed(2))
+    document.getElementById("cantidad").value = valori.result.toFixed(2)
+    
+}
+async function calcularCambio(){
+    const uri = "https://api.exchangerate.host/convert?from="+moneda1.value+"&to="+moneda2.value+"&amount="+cantidad.value
+    const valor = await valormoneda(uri)
+    render(valor)
+}
+calcularCambio()
+moneda1.addEventListener('change',async ()=>{
+    const uri = "https://api.exchangerate.host/convert?from="+moneda1.value+"&to="+moneda2.value+"&amount="+cantidad.value
+    const monedas = await valormoneda(uri)
+    render(monedas)
+})
+moneda2.addEventListener('change',async ()=>{
+    const uri = "https://api.exchangerate.host/convert?from="+moneda1.value+"&to="+moneda2.value+"&amount="+cantidad.value
+    const monedas = await valormoneda(uri)
+    render(monedas)
+})
+cantidad.addEventListener('input',async ()=>{
+    const uri = "https://api.exchangerate.host/convert?from="+moneda1.value+"&to="+moneda2.value+"&amount="+cantidad.value
+    const monedas = await valormoneda(uri)
+    render(monedas)
+})
+cantidad2.addEventListener('input',async ()=>{
+    const uri = "https://api.exchangerate.host/convert?from="+moneda2.value+"&to="+moneda1.value+"&amount="+cantidad2.value
+    const monedas = await valormoneda(uri)
+    render2(monedas)
+})
+
 function removeAllChilds(a)
  {
- var a=document.getElementById(a);
+ a = document.getElementById(a);
  while(a.hasChildNodes())
 	a.removeChild(a.firstChild);	
  }
 const contenedor = document.getElementById("res")
-const btn = document.getElementById("calcular")
-btn.addEventListener('click',(evt)=>{
-    evt.preventDefault()
+const btn = document.querySelector("#guardar")
+btn.addEventListener('click',async ()=>{
     removeAllChilds("res")
-    moneda1 = document.getElementById("monedaUno").value
-    moneda2 = document.getElementById("monedaDos").value
-    cantidad = document.getElementById("cantidad").value
-    buscarmoneda()
-    let items= cantidad + " " + moneda1 + " a " + moneda2 + " = " +resultadofinal+moneda2
+    const uri = "https://api.exchangerate.host/convert?from="+moneda1.value+"&to="+moneda2.value+"&amount="+cantidad.value
+    const monedas = await valormoneda(uri)
+    const resultadofinal = monedas.result.toFixed(2)
+    let items= cantidad.value + " " + moneda1.value + " a " + moneda2.value + " = " +resultadofinal+moneda2.value
     localStorage.setItem("resultados"+x,items)
     x+=1
     if(localStorage.length != 0){
@@ -61,7 +82,7 @@ if(localStorage.length != 0){
 }
 
 const limpiar = document.getElementById("botonLimpiar")
-limpiar.addEventListener('click', (evt)=>{
+limpiar.addEventListener('click', ()=>{
     localStorage.clear()
     removeAllChilds("res")
     x = 0
